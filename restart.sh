120d6c1bf16a517f5b4269a7e9f3222b892b14d8
KnifeZ <KnifeZ@qq.com>
2023-12-25 13:45:53 +0800

    fixed

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pid=$(ps -ef| grep fridayboot-api-0.0.1-SNAPSHOT.jar|grep -v grep|awk '{print $2}')
+pid=$(ps -ef| grep fridayboot-host.jar|grep -v grep|awk '{print $2}')
 kill ${pid}
-nohup /usr/local/btjdk/openjdk17/bin/java -jar -Xmx1024M -Xms256M ./fridayboot-api-0.0.1-SNAPSHOT.jar --server.port=8026 --spring.profiles.active=prod &
+nohup /usr/local/btjdk/openjdk17/bin/java -jar -Xmx1024M -Xms256M ./fridayboot-host.jar --server.port=8026 --spring.profiles.active=prod &
